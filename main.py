f2395f77710e70aab8e442625500c553f598ac4e
Chris Williamson <cjwilliamson109@gmail.com>
2025-10-13 20:33:02 +0100

    Removed unused imports from main

--- main.py
+++ main.py
@@ -1,25 +1,10 @@
 import json
-from typing import Callable, List, Tuple
-import pygame
-import random
+from typing import Callable, List
 from automata.automaton import Automaton
 from automata.binary.builder import binary_automata_from_config
-from automata.brians_brain.brians_brain_automaton import BriansBrainAutomaton
 from automata.brians_brain.builder import brians_brain_automata_from_config
-from automata.cyclic_cellular_automata import CyclicAutomaton, CyclicGridGenerator
 from automata.forest_fire.builder import forest_fire_automata_from_config
 import automata_runner
-from grid_generator.binary_grid_generator import BinaryGridGenerator
-from grid_generator.brians_brain_grid_generator import BriansBrainGridGenerator
-from helpers import get_random_odd_number
-
-# def cyclic() -> Automaton:
-#     number_of_states = 10
-#     grid_generators = [
-#         CyclicGridGenerator(screen_size[0] + 1, (screen_size[0] / 2, screen_size[1] / 2), 1, number_of_states),
-#     ]
-#     automaton = CyclicAutomaton(grid_generators, 'Cyclic', screen_size, number_of_states)
-#     return automaton
 
 AUTOMATA_SIZE = (500, 500) # The size that is used to simulate the automata, this is not the final output size.
 game_size = AUTOMATA_SIZE
